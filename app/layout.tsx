854ac7ba09f7e0485623d68d49c4f17baf967b9f
purvaz <pzinjarde@sfsu.edu>
2025-08-15 11:25:56 -0700

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { Wix_Madefor_Text } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 // const geistSans = Geist({
@@ -38,6 +39,7 @@ export default function RootLayout({
         className={`${wixFont.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
